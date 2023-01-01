ae2fd8dc95063a8d2be2e0904d3063328f135aac
netrialiarahmi <86939625+netrialiarahmi@users.noreply.github.com>
2023-01-01 15:53:46 +0800

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'admin.admin-dashboard-component' => 'App\\Http\\Livewire\\Admin\\AdminDashboardComponent',
+  'cart-component' => 'App\\Http\\Livewire\\CartComponent',
+  'checkou-component' => 'App\\Http\\Livewire\\CheckouComponent',
+  'checkout-component' => 'App\\Http\\Livewire\\CheckoutComponent',
+  'home-component' => 'App\\Http\\Livewire\\HomeComponent',
+  'shop-component' => 'App\\Http\\Livewire\\ShopComponent',
+  'user.user-dashboard-component' => 'App\\Http\\Livewire\\User\\UserDashboardComponent',
+);
